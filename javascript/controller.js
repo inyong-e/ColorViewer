25d01763acd0a4e8bbc1b9640ebcaf63b4e519ab
inyong-e <jiy8319@gmail.com>
2020-07-19 10:36:37 +0900

    FEATURE: 현재의 웹 페이지에 대한 DOM을 가져오는 기능 구현.
    익스텐션 팝업에서 현재의 웹 페이지에 접근하기 위해서 manifest.json의 permissions 값을 변경했다.
    웹 페이지에 접근할 때는 비동기 처리가 되기 때문에 Promise를 이용했다.

--- javascript/controller.js
+++ javascript/controller.js
@@ -1,6 +1,24 @@
-const onClickExport = () => {
-  alert("1111 ");
+const getBodyDOM = () => {
+  return new Promise((resolve, reject) => {
+    try {
+      chrome.tabs.executeScript(
+        {
+          code: 'document.querySelector("body")',
+        },
+        function (result) {
+          resolve(result[0]);
+        },
+      );
+    } catch (error) {
+      reject(error);
+    }
+  });
 };
 
-const exportBtn = document.getElementById("export-color-btn");
+const onClickExport = async () => {
+  const a = await getBodyDOM();
+  alert(a);
+};
+
+const exportBtn = document.getElementById("export-btn");
 exportBtn.addEventListener("click", onClickExport);
